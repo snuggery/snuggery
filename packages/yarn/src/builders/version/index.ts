578161b5d98630ef285e88876d21296d6656f0b5
Bram Gotink <bram@gotink.me>
2021-11-22 23:00:17 +0100

    {fix} limit exposed output to JSON compatible data

--- packages/yarn/src/builders/version/index.ts
+++ packages/yarn/src/builders/version/index.ts
@@ -1,8 +1,14 @@
-import {createBuilder} from '@angular-devkit/architect';
+import {BuilderContext, createBuilder} from '@angular-devkit/architect';
+import type {JsonObject} from '@angular-devkit/core';
+import {mapSuccessfulResult} from '@snuggery/architect/operators';
 
 import {executeVersion} from './executor';
 
 export {VersionBuilderOutput} from './yarn';
 export {executeVersion};
 
-export default createBuilder(executeVersion);
+export default createBuilder((options: JsonObject, context: BuilderContext) =>
+	executeVersion(options, context).pipe(
+		mapSuccessfulResult(({success}) => ({success})),
+	),
+);
