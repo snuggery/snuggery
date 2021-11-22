55d49e705f2845b4d944db29f39c4b012eccaa7d
Bram Gotink <bram@gotink.me>
2021-11-22 22:53:10 +0100

    {feature} add yarn's ESM loader to the global package

---
+++ packages/global/NOTICE.md
@@ -0,0 +1,8 @@
+# Copyright notices
+
+## ESM Loader
+
+This package contains `.pnp.loader.mjs`, an ES module loader for node.js built by the Yarn team.
+
+Copyright (c) 2016-present, Yarn Contributors. All rights reserved.
+Licensed under the BSD 2-Clause License, https://github.com/yarnpkg/berry/blob/83311e1d4d/LICENSE.md
---
+++ packages/global/bin.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if readlink -f "$0" >/dev/null 2>&1; then
+	# GNU readlink is installed (available on all GNU/Linux distributions as part of GNU's coreutils)
+	rp=$(readlink -f -- "$0")
+elif command -v python >/dev/null 2>&1; then
+	# Fall back to python (pre-installed on macOS)
+	# cspell:disable-next-line
+	rp=$(python -c 'import os,sys;print(os.path.realpath(os.path.expanduser(sys.argv[1])))' "$0")
+elif command -v perl >/dev/null 2>&1; then
+	# Fall back to perl (also pre-installed on macOS)
+	# cspell:disable-next-line
+	rp=$(perl -MCwd -e 'print Cwd::abs_path shift' "$0")
+else
+	# Nothing to fall back to
+	echo 'sn: GNU readlink, python, or perl are required' >&2
+	exit 1
+fi
+
+dir=$(dirname -- "$rp")
+
+exec node --experimental-loader "$dir/.pnp.loader.mjs" "$dir/bin.js" "$@"
