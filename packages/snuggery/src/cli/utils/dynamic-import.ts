5d04a11d801dfc87f521f15a4a03e43ab4322648
Bram Gotink <bram@gotink.me>
2024-05-20 23:16:36 +0200

    {fix} improve handling of partial paths in builder/collection files

--- packages/snuggery/src/cli/utils/dynamic-import.ts
+++ packages/snuggery/src/cli/utils/dynamic-import.ts
@@ -5,18 +5,8 @@ const require = createRequire(import.meta.url);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function dynamicImport(path: string): Promise<any> {
-	try {
-		// Import requires valid (partial) URLs, e.g. `./lorem` or `/ipsum`,
-		// but absolute DOS-style paths aren't valid: D:/dolor or even D:\sit
-		return await import(pathToFileURL(path).href);
-	} catch (e) {
-		if (
-			e instanceof Error &&
-			(e as NodeJS.ErrnoException).code === "ERR_MODULE_NOT_FOUND"
-		) {
-			return require(path);
-		}
-
-		throw e;
-	}
+	// Import requires valid (partial) URLs, e.g. `./lorem` or `/ipsum`,
+	// but absolute DOS-style paths aren't valid: D:/dolor or even D:\sit,
+	// so pass it through pathToFileURL
+	return await import(pathToFileURL(require.resolve(path)).href);
 }
